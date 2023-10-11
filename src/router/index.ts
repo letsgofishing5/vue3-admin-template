890a94a8b1a070a8cac11c1df1717b4eae5f106b
走我们去钓鱼 <1113237401@qq.com>
2023-10-11 14:46:23 +0800

    feat: 哈希路由

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from "vue-router"
+import { createRouter, createWebHashHistory, createWebHistory } from "vue-router"
 import { ConstantRoutes, NotFoundRoutes } from "./modules/constantRoutes"
 import NProgress from "nprogress";
 import { useUserStore } from "@/store/user";
 import { initDynamicRoutes } from "./utils";
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         ...ConstantRoutes,
         ...NotFoundRoutes
